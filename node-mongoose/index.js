1980f7523b4069f3478b79f481ad19b0aba08183
h-ARTS <hi5hanan@yahoo.com>
2018-01-21 20:49:09 +0100

    mongoose 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -4,34 +4,47 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url, {
-    useMongoClient: true
-});
+const connect = mongoose.connect(url);
 
 connect.then((db) => {
 
     console.log('Connect correctly to the server');
 
-    var newDish = Dishes({
-        name: 'hamburger cheese',
-        description: 'juicy meat'
+    Dishes.create({
+        name: 'Donut Pink',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling! Its soo yummy!!!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-            
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) =>  {
-            console.log(dishes);
-
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
