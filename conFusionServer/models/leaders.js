719139c83003e13160b12d4f31fb9b7e4ff65af7
h-ARTS <hi5hanan@yahoo.com>
2018-01-26 16:12:50 +0100

    Assigment 2

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  designation: {
+    type: String,
+    required: true
+  },
+  abbr: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true,
+  collection: 'leadership'
+});
+
+const Leader = mongoose.model('Leadership', leaderSchema);
+
+module.exports = Leader;
